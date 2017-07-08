55dc83b7b3018f6d70bdb9874e6ddc04427112c7
Ichika <e@kleop.com>
2017-07-08 18:26:09 +0300

    New engine, fixed #28, #21, #24, #26

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -9,29 +9,38 @@ from threading import Timer
 
 __addon__ = xbmcaddon.Addon("script.simkl")
 
-def getstr(strid):
+
+def log(s):
+    xbmc.log("-- Simkl: {0}".format(s), level=xbmc.LOGDEBUG)
+
+
+def get_str(strid):
     """ Given an id, returns the localized string """
     return __addon__.getLocalizedString(strid)
 
-def getSetting(settingid):
+
+def get_setting(settingid):
     """ Given an id, return the setting """
     ret = __addon__.getSetting(settingid)
-    xbmc.log("Simkl: {0}: {1}".format(settingid, ret))
-    if ret == "false": ret = False
+    log("get setting {0} = {1}".format(settingid, ret))
     return ret
 
-def systemLock(name):
+
+def set_setting(settingid, val):
+    """ Given an id, return the setting """
+    log("set setting, {0} = {1}".format(settingid, val))
+    __addon__.setSetting(settingid, val)
+
+
+def system_lock(name, sec=0):
     w = xbmcgui.Window(10000)
     if w.getProperty(name) == "True":
-        xbmc.log('Simkl: already started, ' + name)
+        log('already started, ' + name)
         sys.exit(0)
     w.setProperty(name, "True")
+    if sec != 0:
+        def stop_singleton():
+            w.clearProperty(name)
 
-def systemUnlockDelay(name,delay):
-    w = xbmcgui.Window(10000)
-
-    def stop_singleton():
-        w.clearProperty(name)
-
-    t = Timer(delay, stop_singleton)
-    t.start()
+        t = Timer(sec, stop_singleton)
+        t.start()
